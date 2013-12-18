6c7055b3cc5e49a41dba57ed3277a8402e361019
8c6794b6 <8c6794b6@gmail.com>
2013-12-19 01:07:25 +0900

    Add multi-dimensional FFT functions

--- Data/Array/Repa/FFTW.hs
+++ Data/Array/Repa/FFTW.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-|
 Module      : $Header$
 CopyRight   : (c) 8c6794b6, 2011, 2012
@@ -11,7 +8,8 @@ Portability : portable
 
 Performs fft of repa array data via FFTW.
 
-Currently supporting DIM1 (Complex Double) arrays only.
+Currently supporting ('Complex' Double) arrays for dimensions 'DIM1', 'DIM2',
+and 'DIM3' only.
 
 -}
 
@@ -19,13 +17,21 @@ module Data.Array.Repa.FFTW
   ( -- * Examples
     -- $examples
 
+    -- * Multi dimension functions
+    -- $multi
+
     -- * References
     -- $references
 
     -- * FFT functions (1 dimension)
     fft
   , ifft
-
+    -- * FFT functions (2 dimension)
+  , fft2d
+  , ifft2d
+    -- * FFT functions (3 dimension)
+  , fft3d
+  , ifft3d
   ) where
 
 import Data.Complex (Complex(..))
@@ -34,7 +40,7 @@ import Foreign.Storable (Storable(..))
 import System.IO.Unsafe (unsafePerformIO)
 
 import Data.Array.CArray (CArray)
-import Data.Array.Repa ((:.)(..), Array, DIM1, Z(..))
+import Data.Array.Repa ((:.)(..), Array, DIM1, DIM2, DIM3, Z(..))
 import Data.Array.Repa.Repr.ForeignPtr (F)
 import Foreign.Storable.Complex ()
 
@@ -54,19 +60,25 @@ Sample module:
 > import Data.Array.Repa.FFTW
 >
 > a :: Array F DIM1 (Complex Double)
-> a = fromList (Z :. 5) [i :+ 0 | i <- [0..4]]
+> a = fromList (Z :. 4) [i :+ 0 | i <- [0..3]]
 
 Loading above in ghci:
 
 >>> toList a
-[0.0 :+ 0.0,1.0 :+ 0.0,2.0 :+ 0.0,3.0 :+ 0.0,4.0 :+ 0.0]
+[0.0 :+ 0.0,1.0 :+ 0.0,2.0 :+ 0.0,3.0 :+ 0.0]
 >>> toList $ fft a
-[10.0 :+ 0.0,(-2.5) :+ 3.4409548011779334
-,(-2.5) :+ 0.8122992405822659
-,(-2.5) :+ (-0.8122992405822659)
-,(-2.5) :+ (-3.4409548011779334)]
+[6.0 :+ 0.0,(-2.0) :+ 2.0,(-2.0) :+ 0.0,(-2.0) :+ (-2.0)]
 >>> toList $ ifft $ fft a
-[0.0 :+ 0.0,1.0000000000000002 :+ 0.0,2.0 :+ 0.0,3.0 :+ 0.0,4.0 :+ 0.0]
+[0.0 :+ 0.0,1.0 :+ 0.0,2.0 :+ 0.0,3.0 :+ 0.0]
+
+-}
+
+{-$multi
+
+Although FFTW library has more flexiblity, choice of dimensions in
+multi-dimension FFT functions ('fft2d', 'ifft2d' ... etc) were using fixed
+dimensions to perform FFTs. Those choices were made after @fft2dP@, @fft3dP@
+functions from @repa-algorithms@ package.
 
 -}
 
@@ -76,11 +88,12 @@ Loading above in ghci:
 
 * fftw haskell binding : <http://hackage.haskell.org/package/fft>
 
+* repa-algorithms: <http://hackage.haskell.org/package/repa-algorithms>
+
 -}
 
 -- --------------------------------------------------------------------------
 -- Exposed functions
---
 
 -- | Performs 1 dimension forward fft.
 fft :: Array F DIM1 (Complex Double) -> Array F DIM1 (Complex Double)
@@ -91,6 +104,26 @@ fft = c2r . FFT.dft . r2c
 ifft :: Array F DIM1 (Complex Double) -> Array F DIM1 (Complex Double)
 ifft = c2r . FFT.idft . r2c
 {-# INLINE ifft #-}
+
+-- | Performs 2 dimension forward fft.
+fft2d :: Array F DIM2 (Complex Double) -> Array F DIM2 (Complex Double)
+fft2d = c2r2d . FFT.dftN [0,1] . r2c2d
+{-# INLINE fft2d #-}
+
+-- | Performs 2 dimension inverse fft.
+ifft2d :: Array F DIM2 (Complex Double) -> Array F DIM2 (Complex Double)
+ifft2d = c2r2d . FFT.idftN [0,1] . r2c2d
+{-# INLINE ifft2d #-}
+
+-- | Performs 3 dimension forward fft.
+fft3d :: Array F DIM3 (Complex Double) -> Array F DIM3 (Complex Double)
+fft3d = c2r3d . FFT.dftN [0,1,2] . r2c3d
+{-# INLINE fft3d #-}
+
+-- | Performs 3 dimension inverse fft.
+ifft3d :: Array F DIM3 (Complex Double) -> Array F DIM3 (Complex Double)
+ifft3d = c2r3d . FFT.idftN [0,1,2] . r2c3d
+{-# INLINE ifft3d #-}
 
 -- --------------------------------------------------------------------------
 -- Guts
@@ -109,3 +142,39 @@ c2r carr = case C.toForeignPtr carr of
     unsafePerformIO $ withForeignPtr fptr $ \ptr ->
     peekElemOff ptr $ R.toIndex sh ix
 {-# INLINE c2r #-}
+
+r2c2d :: Array F DIM2 (Complex Double) -> CArray (Int, Int) (Complex Double)
+r2c2d rarr = unsafePerformIO $ do
+    let _:.n1:.n2 = R.extent rarr
+        fptr = RF.toForeignPtr rarr
+    C.unsafeForeignPtrToCArray fptr ((0,0), (n1-1, n2-1))
+{-# INLINE r2c2d #-}
+
+c2r2d :: CArray (Int, Int) (Complex Double) -> Array F DIM2 (Complex Double)
+c2r2d carr = case C.toForeignPtr carr of
+    (n, fptr) ->
+        let sh = Z:.n':.n'
+            n' = ceiling $ (sqrt $ fromIntegral n :: Double)
+        in  R.computeS $ R.fromFunction sh $ \ix ->
+            unsafePerformIO $ withForeignPtr fptr $ \ptr ->
+            peekElemOff ptr $ R.toIndex sh ix
+{-# INLINE c2r2d #-}
+
+r2c3d :: Array F DIM3 (Complex Double)
+      -> CArray (Int, Int, Int) (Complex Double)
+r2c3d rarr = unsafePerformIO $ do
+    let _:.n1:.n2:.n3 = R.extent rarr
+        fptr = RF.toForeignPtr rarr
+    C.unsafeForeignPtrToCArray fptr ((0,0,0), (n1-1, n2-1, n3-1))
+{-# INLINE r2c3d #-}
+
+c2r3d :: CArray (Int, Int, Int) (Complex Double)
+      -> Array F DIM3 (Complex Double)
+c2r3d carr = case C.toForeignPtr carr of
+    (n, fptr) ->
+        let sh = Z:.n':.n':.n'
+            n' = ceiling $ fromIntegral n ** (1/3 :: Double)
+        in  R.computeS $ R.fromFunction sh $ \ix ->
+            unsafePerformIO $ withForeignPtr fptr $ \ptr ->
+            peekElemOff ptr $ R.toIndex sh ix
+{-# INLINE c2r3d #-}
--- exec/test.hs
+++ exec/test.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.Array.Repa ((:.)(..), Array, DIM1, Z(..), toList)
+import Data.Array.Repa ((:.)(..), Array, DIM1, DIM2, DIM3, Shape, Z(..), toList)
 import Data.Array.Repa.Eval (fromList)
 import Data.Array.Repa.Repr.ForeignPtr (F)
 import Data.Complex (Complex(..), magnitude)
@@ -11,34 +11,84 @@ import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (Assertion, assertBool, testCase)
 import Test.Tasty.QuickCheck (Property, choose, forAll, testProperty, vector)
 
-import Data.Array.Repa.FFTW (fft, ifft)
+import Data.Array.Repa.FFTW (fft, ifft, fft2d, ifft2d, fft3d, ifft3d)
 
 
 main :: IO ()
 main = defaultMain $
     testGroup "repa-fftw"
     [ testCase "magsum1d-hunit" case_magsum1d
+    , testCase "magsum2d-hunit" case_magsum2d
+    , testCase "magsum3d-hunit" case_magsum3d
     , testProperty "magsum1d-qc" prop_magsum1d
+    , testProperty "magsum2d-qc" prop_magsum2d
+    , testProperty "magsum3d-qc" prop_magsum3d
     ]
 
 case_magsum1d :: Assertion
 case_magsum1d = do
-    let a = fromList (Z :. 5) [i :+ 0 | i <- [0..4]]
-    assertBool "diff is greater than epsilon" $ compareMagnitudeSum a
+    let a = fromList (Z :. 4) [i :+ 0 | i <- [0..3]]
+    assertBool "diff is greater than epsilon" $ compareMagnitudeSum1d a
+
+case_magsum2d :: Assertion
+case_magsum2d = do
+    let a = fromList (Z :. 4 :. 4) [i :+ 0 | i <- [0..15]]
+    assertBool "diff is greater than epsilon" $ compareMagnitudeSum2d a
+
+case_magsum3d :: Assertion
+case_magsum3d = do
+    let a = fromList (Z :. 4 :. 4 :. 4) [i :+ 0 | i <- [0..63]]
+    assertBool "diff is greater than epsilon" $ compareMagnitudeSum3d a
 
 prop_magsum1d :: Property
 prop_magsum1d =
     forAll (choose (1, 16::Int)) $ \pow ->
-    let size = (2 ^ pow) + 1 in
-    forAll (vector size) $ \vals ->
-    compareMagnitudeSum $ fromList (Z :. size) vals
+    let size = (2 ^ pow)
+    in forAll (vector size) $ \vals ->
+       compareMagnitudeSum1d $ fromList (Z :. size) vals
+
+prop_magsum2d :: Property
+prop_magsum2d =
+    forAll (choose (1, 4::Int)) $ \pow ->
+    let extnt = 2 ^ pow
+        size  = extnt ^ (2 :: Int)
+    in  forAll (vector size) $ \vals ->
+        compareMagnitudeSum2d $ fromList (Z :. extnt :. extnt) vals
+
+prop_magsum3d :: Property
+prop_magsum3d =
+    forAll (choose (1, 4::Int)) $ \pow ->
+    let extnt = 2 ^ pow
+        size  = extnt ^ (3 :: Int)
+    in  forAll (vector size) $ \vals ->
+        compareMagnitudeSum3d $ fromList (Z :. extnt :. extnt :. extnt) vals
 
 -- | Performs fft and ifft, then compareing magnitude of sum with the
 -- original.
-compareMagnitudeSum :: Array F DIM1 (Complex Double) -> Bool
-compareMagnitudeSum arr = diff < epsilon
+compareMagnitudeSum1d :: Array F DIM1 (Complex Double) -> Bool
+compareMagnitudeSum1d = compareMagnitudeSum fft ifft
+
+-- | Like 'compareMagnitudeSum', for 2-dimensional array.
+compareMagnitudeSum2d :: Array F DIM2 (Complex Double) -> Bool
+compareMagnitudeSum2d = compareMagnitudeSum fft2d ifft2d
+
+-- | Like 'compareMagnitudeSum', for 3-dimensional array.
+compareMagnitudeSum3d :: Array F DIM3 (Complex Double) -> Bool
+compareMagnitudeSum3d = compareMagnitudeSum fft3d ifft3d
+
+-- | Apply given functions, then compare the result with original.
+compareMagnitudeSum ::
+    (Shape sh)
+    => (Array F sh (Complex Double) -> a)
+    -- ^ Forward FFT function.
+    -> (a -> Array F sh (Complex Double))
+    -- ^ Inverse FFT function.
+    -> Array F sh (Complex Double)
+    -- ^ Original input.
+    -> Bool
+compareMagnitudeSum fn ifn arr = diff < epsilon
   where
-    arr' = ifft $ fft arr
-    mag = magnitude . sum . toList
-    epsilon = 1e-2
     diff = abs (mag arr - mag arr')
+    mag = magnitude . sum . toList
+    arr' = ifn $ fn arr
+    epsilon = 1e-2
